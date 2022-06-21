da2fdfae456d3207376844360f42844ec30bd020
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-20 17:21:07 -0700

    Add files via upload

--- Furthest Building You Can Reach.py
+++ Furthest Building You Can Reach.py
@@ -10,3 +10,18 @@
 # If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
 # Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
 
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        N = len(heights)
+        heap = []
+        for i in range(N-1):
+            h = heights[i + 1] - heights[i]
+            if h <= 0: 
+                continue
+            heappush(heap, h)
+            if len(heap) > ladders:
+                min_h = heappop(heap)
+                bricks -= min_h
+            if bricks < 0:
+                return i
+        return N - 1
